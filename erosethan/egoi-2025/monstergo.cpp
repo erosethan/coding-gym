82744818272066264938cb522cc56d6d054c267e
Ethan Jimenez <ethanjimenezv@outlook.com>
2025-08-11 19:33:18 -0700

    [monstergo] Optimizacion con bitmasks

--- erosethan/egoi-2025/monstergo.cpp
+++ erosethan/egoi-2025/monstergo.cpp
@@ -15,56 +15,52 @@ const vector<pair<int, int>> kBlueprint[50] = {{{1, 1}},
                                                {{4, 3}, {7, 6}},
                                                {{5, 4}, {7, 6}},
                                                {{13, 12}},
-                                               {{13, 12}, {1, 1}},
+                                               {{1, 1}, {13, 12}},
                                                {{6, 4}},
-                                               {{6, 4}, {1, 1}},
+                                               {{1, 1}, {6, 4}},
                                                {{4, 3}, {13, 12}},
                                                {{5, 4}, {13, 12}},
                                                {{4, 3}, {6, 4}},
                                                {{6, 3}},
-                                               {{6, 3}, {1, 1}},
-                                               {{7, 6}, {6, 4}},
+                                               {{1, 1}, {6, 3}},
+                                               {{6, 4}, {7, 6}},
                                                {{5, 3}, {13, 12}},
                                                {{4, 3}, {6, 3}},
                                                {{5, 3}, {6, 4}},
                                                {{4, 2}, {6, 3}},
-                                               {{7, 6}, {6, 3}},
+                                               {{6, 3}, {7, 6}},
                                                {{8, 6}},
-                                               {{8, 6}, {1, 1}},
-                                               {{8, 6}, {2, 1}},
-                                               {{8, 6}, {3, 2}},
-                                               {{8, 6}, {4, 3}},
-                                               {{8, 6}, {5, 4}},
-                                               {{8, 6}, {4, 2}},
+                                               {{1, 1}, {8, 6}},
+                                               {{2, 1}, {8, 6}},
+                                               {{3, 2}, {8, 6}},
+                                               {{4, 3}, {8, 6}},
+                                               {{5, 4}, {8, 6}},
+                                               {{4, 2}, {8, 6}},
                                                {{7, 4}},
-                                               {{7, 4}, {1, 1}},
-                                               {{7, 4}, {2, 1}},
-                                               {{7, 4}, {3, 2}},
-                                               {{7, 4}, {4, 3}},
-                                               {{7, 4}, {5, 4}},
-                                               {{7, 4}, {4, 2}},
-                                               {{7, 4}, {7, 6}},
+                                               {{1, 1}, {7, 4}},
+                                               {{2, 1}, {7, 4}},
+                                               {{3, 2}, {7, 4}},
+                                               {{4, 3}, {7, 4}},
+                                               {{5, 4}, {7, 4}},
+                                               {{4, 2}, {7, 4}},
+                                               {{7, 6}, {7, 4}},
                                                {{6, 4}, {8, 6}},
-                                               {{6, 4}, {8, 6}, {1, 1}},
+                                               {{1, 1}, {6, 4}, {8, 6}},
                                                {{5, 3}, {7, 4}},
-                                               {{5, 4}, {13, 12}, {8, 6}},
-                                               {{5, 4}, {7, 6}, {7, 4}},
+                                               {{5, 4}, {8, 6}, {13, 12}},
+                                               {{5, 4}, {7, 4}, {7, 6}},
                                                {{6, 3}, {8, 6}},
-                                               {{7, 6}, {7, 6}, {7, 4}},
+                                               {{7, 4}, {7, 6}, {7, 6}},
                                                {{6, 4}, {7, 4}}};
 
-int current_begin;
-vector<int> current_list;
-vector<vector<int>> lists;
+int current_begin, group_size;
+vector<long long> lists;
 
-void GenerateCombinations(int n, int r, int group_size) {
-  if (!r) return lists.push_back(current_list);
-  if (r < n) GenerateCombinations(n - 1, r, group_size);
-
-  for (int i = 0; i < group_size; ++i)
-    current_list.push_back(current_begin + group_size * (n - 1) + i);
-  GenerateCombinations(n - 1, r - 1, group_size);
-  for (int i = 0; i < group_size; ++i) current_list.pop_back();
+void GenerateCombinations(int n, int r, long long list) {
+  if (!r) return lists.push_back(list);
+  if (r < n) GenerateCombinations(n - 1, r, list);
+  list |= (1LL << group_size) - 1 << current_begin + group_size * (n - 1);
+  GenerateCombinations(n - 1, r - 1, list);
 }
 
 int main() {
@@ -74,14 +70,15 @@ int main() {
   int n;
   cin >> n;
 
-  for (auto& [total, choose] : kBlueprint[n - 1]) {
-    int group_size = 12 / choose;
-    GenerateCombinations(total, choose, group_size);
+  for (auto [total, choose] : kBlueprint[n - 1]) {
+    group_size = 12 / choose;
+    GenerateCombinations(total, choose, 0);
     current_begin += group_size * total;
   }
 
-  for (auto& list : lists) {
-    for (int Cj : list) cout << Cj << ' ';
+  for (auto list : lists) {
+    for (int i = 1; list; list >>= 1, ++i)
+      if (list & 1) cout << i << ' ';
     cout << '\n';
   }
 }
